9196e0f0f4fff0f9bc42032090fcaccd7a05d5b4
korssa <korssakr@gmail.com>
2025-09-10 20:07:47 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ".translatefeedback.css";
 import { WebsiteSchema, OrganizationSchema } from "@/components/seo-schema";
 
 const geistSans = Geist({
